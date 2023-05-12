d09cc1321717c47e8abfaefce6cc5fe8e86f0251
h <h@DESKTOP-F1IEFU3>
2023-05-12 20:54:23 +0700

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.labelTime = new System.Windows.Forms.Label();
+            this.numericUpDownHours = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownMinutes = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownSeconds = new System.Windows.Forms.NumericUpDown();
+            this.buttonSetAlarm = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownHours)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMinutes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSeconds)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.Location = new System.Drawing.Point(370, 90);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(52, 13);
+            this.labelTime.TabIndex = 0;
+            this.labelTime.Text = "labelTime";
+            // 
+            // numericUpDownHours
+            // 
+            this.numericUpDownHours.Location = new System.Drawing.Point(451, 90);
+            this.numericUpDownHours.Name = "numericUpDownHours";
+            this.numericUpDownHours.Size = new System.Drawing.Size(150, 20);
+            this.numericUpDownHours.TabIndex = 1;
+            // 
+            // numericUpDownMinutes
+            // 
+            this.numericUpDownMinutes.Location = new System.Drawing.Point(451, 129);
+            this.numericUpDownMinutes.Name = "numericUpDownMinutes";
+            this.numericUpDownMinutes.Size = new System.Drawing.Size(150, 20);
+            this.numericUpDownMinutes.TabIndex = 1;
+            // 
+            // numericUpDownSeconds
+            // 
+            this.numericUpDownSeconds.Location = new System.Drawing.Point(451, 173);
+            this.numericUpDownSeconds.Name = "numericUpDownSeconds";
+            this.numericUpDownSeconds.Size = new System.Drawing.Size(150, 20);
+            this.numericUpDownSeconds.TabIndex = 1;
+            // 
+            // buttonSetAlarm
+            // 
+            this.buttonSetAlarm.Location = new System.Drawing.Point(399, 246);
+            this.buttonSetAlarm.Name = "buttonSetAlarm";
+            this.buttonSetAlarm.Size = new System.Drawing.Size(138, 23);
+            this.buttonSetAlarm.TabIndex = 2;
+            this.buttonSetAlarm.Text = "buttonSetAlarm";
+            this.buttonSetAlarm.UseVisualStyleBackColor = true;
+            this.buttonSetAlarm.Click += new System.EventHandler(this.buttonSetAlarm_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonSetAlarm);
+            this.Controls.Add(this.numericUpDownSeconds);
+            this.Controls.Add(this.numericUpDownMinutes);
+            this.Controls.Add(this.numericUpDownHours);
+            this.Controls.Add(this.labelTime);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Hello world";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownHours)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMinutes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSeconds)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.NumericUpDown numericUpDownHours;
+        private System.Windows.Forms.NumericUpDown numericUpDownMinutes;
+        private System.Windows.Forms.NumericUpDown numericUpDownSeconds;
+        private System.Windows.Forms.Button buttonSetAlarm;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class MainForm : Form
+    {
+        private DateTime alarmTime;
+        private NotifyIcon notifyIcon;
+        
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            // Create the NotifyIcon control with an icon and a context menu
+            notifyIcon = new NotifyIcon();
+            notifyIcon.Icon = new Icon("app_icon.ico");
+            notifyIcon.Visible = true;
+
+            ContextMenuStrip contextMenu = new ContextMenuStrip();
+            contextMenu.Items.Add("Show", null, (s, e) => this.Show());
+            contextMenu.Items.Add("Exit", null, (s, e) => Application.Exit());
+            notifyIcon.ContextMenuStrip = contextMenu;
+        }
+
+        //private void timer1_Tick(object sender, EventArgs e)
+        //{
+        //    // Update the time label every second
+        //    labelTime.Text = DateTime.Now.ToString("HH:mm:ss");
+
+        //    // Check if the current time matches the alarm time
+        //    if (DateTime.Now >= alarmTime)
+        //    {
+        //        // Play a sound and show a message box
+        //        //SoundPlayer player = new SoundPlayer(Properties.Resources.alarm_sound);
+        //        //player.Play();
+        //        MessageBox.Show("Alarm!");
+        //    }
+        //}
+
+        private void buttonSetAlarm_Click(object sender, EventArgs e)
+        {
+            // Parse the user input and set the alarm time
+            int hours = (int)numericUpDownHours.Value;
+            int minutes = (int)numericUpDownMinutes.Value;
+            int seconds = (int)numericUpDownSeconds.Value;
+            alarmTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, hours, minutes, seconds);
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            // Hide the form and move it to the system tray instead of closing it
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+                this.Hide();
+                notifyIcon.ShowBalloonTip(1000, "Clock Alarm", "The app is running in the background.", ToolTipIcon.Info);
+            }
+            else
+            {
+                base.OnFormClosing(e);
+            }
+        }
+    }
+}
